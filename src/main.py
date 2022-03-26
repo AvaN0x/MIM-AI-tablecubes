0057dc2b947385abb0ff59c0f77680077c528c52
AvaN0x <clement.ricatte57@gmail.com>
2022-03-26 19:53:16 +0100

    fix: :bug: fix h1

--- src/main.py
+++ src/main.py
@@ -14,45 +14,48 @@ from astar import astar
 
 
 def h1(table):
+    """Heuristic 1 for A* : h(x) = 6 - (ON(A,B) + ON(B,C) + ONTABLE(C))"""
     res = 6
     if (table.A.onCube(table.B)):
-        res += 1
-    else:
         res -= 1
+    else:
+        res += 1
 
     if (table.B.onCube(table.C)):
-        res += 2
-    else:
         res -= 2
-
-    if (table.C.free):
-        res += 3
     else:
+        res += 2
+
+    if (table.C.onTable()):
         res -= 3
+    else:
+        res += 3
 
     return res
 
 
 def g1(table):
     # TODO: implement
-    pass
+    return -1
 
 
 def h2(table):
     # TODO: implement
-    pass
+    return -1
 
 
 def g2(table):
     # TODO: implement
-    pass
+    return -1
 
 
 tableStart = Table.getTableSubjectStart()
-print(tableStart)
+print("tableStart:", tableStart,
+      ", h1=" + str(h1(tableStart)), ", g1=" + str(g1(tableStart)))
 tableStart.draw()
 tableGoal = Table.getTableSubjectGoal()
-print(tableGoal)
+print("tableGoal:", tableGoal,
+      ", h1=" + str(h1(tableGoal)), ", g1=" + str(g1(tableGoal)))
 tableGoal.draw()
 
 # tree1 = astar(h1, g1,
